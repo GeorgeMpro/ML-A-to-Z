d34f51ac682329b508345530fa26b687a29d2f03
george <George.mpro87@gmail.com>
2022-12-31 15:11:28 +0200

    Initial commit

---
+++ data_preprocessing/data_preprocessing_tools.py
@@ -0,0 +1,28 @@
+#### Data Preprocessing Tools
+
+##### Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+##### Importing the dataset
+###
+# we create the data frame
+dataset = pd.read_csv('Data.csv')
+# features - The Columns with which we are going to predict the Dependent Variable
+# For this we create the Matrix of Features
+x = da
+# Dependent Variable Vector - The last column (purchased yes/no)
+
+
+##### Taking care of missing data
+
+##### Encoding categorical data
+
+###### Encoding the Independent Variable
+
+###### Encoding the Dependent Variable
+
+##### Splitting the dataset into the Training set and Test set
+
+##### Feature Scaling
