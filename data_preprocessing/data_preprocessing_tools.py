116cbc357a446e91b0d87a3ab046bd2ffafc0bc2
george <George.mpro87@gmail.com>
2022-12-31 19:40:50 +0200

    Done importing data and class Start taking care of missing data

--- data_preprocessing/data_preprocessing_tools.py
+++ data_preprocessing/data_preprocessing_tools.py
@@ -9,11 +9,20 @@ import pandas as pd
 ###
 # we create the data frame
 dataset = pd.read_csv('Data.csv')
+
 # features - The Columns with which we are going to predict the Dependent Variable
 # For this we create the Matrix of Features
-x = da
-# Dependent Variable Vector - The last column (purchased yes/no)
+# How To:
+# [:] range without upper/lower bounds means to take all the rows; [:,] which columns we want to select with the indexes; [:, :-1] means all the columns except the last column who has index is -1
+# In short, these are the Independent Variables
+x = dataset.iloc[:, :-1].values
 
+# Dependent Variable Vector - The last column (purchased yes/no)
+# We only want the last column, so we remove the range
+y = dataset.iloc[:, -1].values
+
+print(x)
+print(y)
 
 ##### Taking care of missing data
 
