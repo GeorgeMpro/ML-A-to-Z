abb23cb78dc2ef18eabf8cf297f2bc8f68c8f28c
george <George.mpro87@gmail.com>
2023-01-06 21:40:34 +0200

    Encoding the Data and variables

--- data_preprocessing/data_preprocessing_tools.py
+++ data_preprocessing/data_preprocessing_tools.py
@@ -4,6 +4,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+# Need scikit-learn instead of sklearn in the project > python interpreter
+from sklearn.impute import SimpleImputer
+
+# We will use these tools to transform the values of the columns into numbers
+# in addition, we will try to remove connection or order between them - no order between the countries
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 ##### Importing the dataset
 ###
@@ -25,13 +33,40 @@ print(x)
 print(y)
 
 ##### Taking care of missing data
+# we will use the library to deal with missing data
+
+# We will replace the missing salary by the average of salaries
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+# now we apply the imputer on the matrix of features. We want to access columns with numbers only - age, salary.
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+print(x)
 
 ##### Encoding categorical data
 
 ###### Encoding the Independent Variable
 
-###### Encoding the Dependent Variable
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])],
+                       remainder='passthrough')  # we want o keep the rest of the columns
+# Will return the first column with One Hot Encoding
+# We need to turn the ct object to a numpy array which is expected by the machine learning models
+x = np.array(ct.fit_transform(x))
+# Notice: each country is encoded as a vector of size 3; with no numerical order between them
+print('\nEncode countries into a vectors of size 3, made out of 0,1 and without order between them')
+print(x)
 
+# label encode yes/no to 1/0
+le = LabelEncoder()
+y = le.fit_transform(y)
+print('\nEncode Yes/No to 1/0')
+print(y)
+
+
+# We apply the Feature scaling AFTER splitting the dataset into the Training and Test sets
 ##### Splitting the dataset into the Training set and Test set
+
+
 
 ##### Feature Scaling
